ce164bb5c5de3dbfe894304f9a5ec1524d6ae9c8
anuanu7688-dot <anuanu7688@gmail.com>
2026-09-25 20:58:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ if submit:
 
     with st.spinner("N3Bee AI is creating your plan..."):
         try:
-            response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt_text)
+            response = client.models.generate_content(model="gemini-1.5-flash", contents=prompt_text)
             if response.text:
                 st.success("Your Personalized Diet Plan is Ready!")
                 st.markdown(response.text)
@@ -69,4 +69,4 @@ if submit:
                 st.error(f"Error: {e}")
 
 st.divider()
-st.caption("Made with love by Anusri | N3Bee | Powered by Gemini 2.5 Flash")
+st.caption("Made with love by Team N3Bee - Anushree P , Ahammed Sha , Avinth Atchai C , Afsal A | Powered by Gemini 1.5 Flash")
